fa4e5577cca4587b7813bd1b5ed6f6924a9480bc
Safolah / Safi <69314363+safi1997@users.noreply.github.com>
2023-02-02 01:11:24 +0100

    This is Assignment for week 6-2 is unsolved

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+function App() {
+  return (
+    <>
+      <h4>I Wana Solve the Assignment for week6-2, thi si for main Branch!</h4>
+    </>
+  );
+}
+
+export default App;
